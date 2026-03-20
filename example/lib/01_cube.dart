731e7e34ea7ff9d5379d469f290b10a2d2bfb586
Macbear Chen <5847542+macbearchen@users.noreply.github.com>
2026-03-20 23:38:01 +0800

    0.7.1 ready to publish

--- example/lib/01_cube.dart
+++ example/lib/01_cube.dart
@@ -34,7 +34,9 @@ Click buttons to test examples.
   6. Shadow for large scene
   7. Physics scene
   8. Text 3D scene
-  9. PBR Test scene''';
+  9. PBR Test scene
+  10. Terrain scene
+  11. BVH scene''';
     return fm.Positioned(
       top: 50,
       left: 8,
